07f8268224366324671cafb2cb2c2bdd4c07e99d
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2015-10-01 09:15:58 +0900

    LTネタを更新

--- nico_slide/js/comment.js
+++ nico_slide/js/comment.js
@@ -5,28 +5,36 @@ $(function(){
     data: {
       comment: '',
       size: '',
-      color: ''
+      color: '',
+      type: ''
     },
 
     created: function(){
-
+      this.comment = '（´・ω・｀）< まあ、押してみそ';
     },
 
     methods:{
 
       onSend: function(e){
-         messageDataStore.push({content : this.comment});
+         messageDataStore.push({content: this.comment, type: 'text'});
       },
 
       onSendLgtm: function(e){
         $.ajax({
           url: "http://lgtm.in/g"
         }).done(function(data){
-          console.dir(data);
-          var re = /( class="<.+ form-control" id="imageUrl" name="imageUrl">)/;
-          console.dir(data.match(re));
+          var lgtmUrl = data.match(/(<.+ class="form-control" id="imageUrl" name="imageUrl">)/);
+          if (0 < lgtmUrl.length) {
+            var url = lgtmUrl[0]
+                        .replace(/^(<input type="text" value=")/, '')
+                        .replace(/(" class="form-control" id="imageUrl" name="imageUrl">)$/, '');
+
+            messageDataStore.push({content: url, type: 'image'});
+          } else {
+            alert('画像がとれなかったよ・・・');
+          }
         }).fail(function(data){
-          alert("つながらないよ！");
+          alert('つながらないよ・・・');
         });
       }
     }
--- nico_slide/js/index.js
+++ nico_slide/js/index.js
@@ -2,44 +2,62 @@ $(function(){
 
   var commentArea = document.getElementById('main-slide');
 
-  var $slideArea = $('#main-slide');
+  // コメント追加時のObserverを作成
   var mo = new MutationObserver(function(mutationRecords){
 
-    console.dir(mutationRecords);
-
+    // Element削除時は何もしない
     if(mutationRecords[0].addedNodes.length === 0) {
       return;
     }
 
-    var moveWidth = ($slideArea.width() - 0) * 1.5;
-    var yPos = Math.floor( Math.random() * $slideArea.height() );
-
+    // commentAreaに追加されたcommentを取得
     var el = mutationRecords[0].addedNodes[0];
+
+    // 初期表示時の位置とアニメーションのスタイルを設定
     el.style.right = '-' + el.offsetWidth + 'px';
-    el.style.top = yPos + 'px';
-    el.style.webkitTransform = 'translate(-' + moveWidth + 'px, 0px)';
+    el.style.top = Math.floor( Math.random() * 90 ) + '%';
+    el.style.webkitTransform = 'translate(-' + (commentArea.offsetWidth * 2) + 'px, 0px)';
     el.style.webkitTransitionDuration = '10s';
+
+    // アニメーションが終わったら消す
     setTimeout(function() {
       commentArea.removeChild(el);
     }, 10000);
-
   });
-  mo.observe($slideArea[0], {childList:true, attributes: false, attributeOldValue: false});
 
+  // コメント表示領域の監視を開始。
+  mo.observe(commentArea, {childList:true, attributes: false, attributeOldValue: false});
+
+  // MilkCocoaからコメントを受け取った時のコールバック
   messageDataStore.on('push', function(pushed) {
 
+    // コメントを表示するElementを作成
     var el = document.createElement('div');
     el.className = 'comment';
-    el.innerHTML = pushed.value.content;
-    commentArea.appendChild(el);
 
-// var image = "http://33.media.tumblr.com/0812c2f2a5aaa0456243cad84ff93a51/tumblr_muolwfW8Hu1re64ggo1_500.gif";
-// var $image = $('<img />', {
-//   src: image,
-//   class: 'comment'
-// });
-//     $slideArea.append($image);
+    if(pushed.value.type === 'image' || pushed.value.content.toLowerCase().search(/^((http|https):\/\/.+(\.jpg|\.gif|\.png))/) === 0) {
+      var image = new Image(150);
+      image.onload = function(e){
+        el.appendChild(this);
+        commentArea.appendChild(el);
+      };
+      image.src = pushed.value.content;
+    } else {
+      el.innerHTML = escapeHtml(pushed.value.content);
+      commentArea.appendChild(el);
+    }
 
     console.dir(pushed);
   });
+
+  // HTMLエスケープ用
+  var escapeHtml = function(str) {
+    str = str.replace(/&/g, '&amp;');
+    str = str.replace(/</g, '&lt;');
+    str = str.replace(/>/g, '&gt;');
+    str = str.replace(/"/g, '&quot;');
+    str = str.replace(/'/g, '&#39;');
+    return str;
+  };
+
 });
